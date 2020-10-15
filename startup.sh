91eba6f3376d785fad48849865fefb36e94c0c1b
David Ruwadi <druwadi13@gmail.com>
2020-10-15 03:04:01 -0400

    fixed startup script to work properly

--- startup.sh
+++ startup.sh
@@ -1,10 +1,10 @@
-echo "Starting up InfluxDB via Docker" \
-docker run -d -p 8086:8086 \
-influxdb:1.8.2 \
-echo "Database started..." \
-curl -i -XPOST http://localhost:8086/query --data-urlencode "q=CREATE DATABASE collector_metrics" \
-echo "Building and running test host" \
-docker build -f host.Dockerfile . -t host \
-docker run -it -d host /bin/bash \
+echo "Starting up InfluxDB via Docker"
+docker run -d -p 8086:8086 influxdb:1.8.2
+echo "Database started..."
+sleep 10
+curl -i -XPOST http://localhost:8086/query --data-urlencode "q=CREATE DATABASE collector_metrics"
+echo "Building and running test host"
+docker build -f host.Dockerfile . -t host
+docker run -it -d host /bin/bash
 echo "Test host is running..."
 
