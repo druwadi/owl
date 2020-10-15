715df1a63c4f9b6b6324688bf80bf9a00b3637ec
David Ruwadi <druwadi13@gmail.com>
2020-10-15 01:44:01 -0400

    adding dicts to format nosql schema for influxDB, adding test host dockerfile, updating startup script

---
+++ collector.py
@@ -0,0 +1,59 @@
+from datetime import datetime
+from influxdb import InfluxDBClient
+import psutil
+import socket
+import json
+
+client = InfluxDBClient(host='localhost', port=8086)
+client.switch_database('collector_metrics')
+
+# defines the hostname where the metrics are running
+# to be used as a tag in the database for querying
+host = socket.gethostname()
+
+while True:
+    # Metrics collected listed here
+    time_val = datetime.now().isoformat(' ')
+    cpu_val = psutil.cpu_percent(interval=1)
+    mem_val = psutil.virtual_memory().percent
+
+    # JSON objects for DB ingestion 
+
+    # CPU Percent Metrics
+    cpu_payload = [
+        {
+        "measurement": "cpu_percent",
+        "tags": {
+            "host": host
+        },
+        "time": time_val,
+        "fields": {
+            "value": cpu_val
+        }
+    }
+    ]
+
+    # Memory Percent Metrics
+    mem_payload = [
+        {
+        "measurement": "mem_percent",
+        "tags": {
+            "host": host
+        },
+        "time": time_val,
+        "fields": {
+            "value": mem_val
+        }
+    }
+    ]
+
+    # Serializing values to JSON (possibly not necessary with dicts)
+    #cpu_payload_json = json.dumps(cpu_payload)
+    #mem_payload_json = json.dumps(mem_payload)
+    
+    # Debug statements
+    #print(cpu_payload)
+    #print(mem_payload)
+
+    client.write_points(cpu_payload)
+    client.write_points(mem_payload)
