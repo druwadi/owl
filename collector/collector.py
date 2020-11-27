2e6abd5eb28e82bd02e3828cafcd0f61872acbca
David Ruwadi <druwadi13@gmail.com>
2020-11-27 04:18:32 -0500

    feat: adding multiple graphs to dashboard and fixing linux build to not use compressed method

--- collector/collector.py
+++ collector/collector.py
@@ -16,8 +16,19 @@ host = socket.gethostname()
 while True:
     # Metrics collected listed here
     time_val = datetime.now().isoformat(' ')
+
+    # CPU Related
     cpu_val = psutil.cpu_percent(interval=1)
+
+    # Memory Related
     mem_val = psutil.virtual_memory().percent
+
+    # Disk Related
+    disk = psutil.disk_usage('/')
+    disk_percent = psutil.disk_usage('/').percent
+    disk_total = (disk.total / (1024.0 ** 3))
+    disk_used = (disk.used / (1024.0 ** 3))
+    disk_free = (disk.free / (1024.0 ** 3))
 
     # JSON objects for DB ingestion 
 
@@ -42,16 +53,71 @@ while True:
         "tags": {
             "host": host
         },
-        "time": time_val,
         "fields": {
             "value": mem_val
         }
     }
     ]
-    
-    # Debug statements
-    #print(cpu_payload)
-    #print(mem_payload)
 
+    # Disk Metrics
+
+    disk_space_percent_payload = [
+        {
+        "measurement": "disk_percent",
+        "tags": {
+            "host": host
+        },
+        "time": time_val,
+        "fields": {
+            "value": disk_percent
+        }
+    }
+    ]
+
+    disk_space_total_payload = [
+        {
+        "measurement": "disk_space_total",
+        "tags": {
+            "host": host
+        },
+        "time": time_val,
+        "fields": {
+            "value": disk_total
+        }
+    }
+    ]
+
+    disk_space_free_payload = [
+        {
+        "measurement": "disk_space_free",
+        "tags": {
+            "host": host
+        },
+        "time": time_val,
+        "fields": {
+            "value": disk_free
+        }
+    }
+    ]
+
+    disk_space_used_payload = [
+        {
+        "measurement": "disk_space_free",
+        "tags": {
+            "host": host
+        },
+        "time": time_val,
+        "fields": {
+            "value": disk_used
+        }
+    }
+    ]
+
+
+# Writing data to influxdb
     client.write_points(cpu_payload)
     client.write_points(mem_payload)
+    client.write_points(disk_space_percent_payload)
+    client.write_points(disk_space_total_payload)
+    client.write_points(disk_space_free_payload)
+    client.write_points(disk_space_used_payload)
